795b076bda47b159d8be5866172ff9476cd2ddb6
b1s0la <yinusabisola@gmail.com>
2017-10-04 12:22:19 +0100

    Bar chart remainig

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 // import {BarChart} from 'react-easy-chart';
 import './index.css';
 import App from './App';
-import {BarChart} from 'react-easy-chart';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
